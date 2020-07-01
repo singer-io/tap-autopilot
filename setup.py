eaf40be7ffdd04ca3180ddbc2e2f03d2be571614
asaf-erlich <aerlich@talend.com>
2020-07-01 16:28:25 -0400

    Bumped version to 0.3.0 and generated changelog (#8)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-autopilot',
-      version='0.2.0',
+      version='0.3.0',
       description='Singer.io tap for extracting data from the Autopilot API',
       author='Stitch',
       author_email='support@stitchdata.com',
